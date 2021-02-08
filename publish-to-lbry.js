a000b72ab107645e969ca861f7d2600981e9bfa9
Ryan Burgett <ryan@burgettdev.net>
2021-02-08 02:03:08 -0500

    Added video images

--- publish-to-lbry.js
+++ publish-to-lbry.js
@@ -135,7 +135,7 @@ const getConfirmations = async function(permanentUrl) {
     // }
     // return;
 
-    for(const episode of episodes.slice(-1)) {
+    for(const episode of episodes.slice(68, 72)) {
 
       const { NUMBER } = episode;
       let numberStr = NUMBER.toString();
